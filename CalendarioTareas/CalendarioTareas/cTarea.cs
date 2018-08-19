b04c16f1fcb7e1d9981c2b66cf20adc04a29b1e3
Angel Jimenez <angeljpp29@gmail.com>
2018-08-18 23:30:00 -0600

    Creación de la clase Tarea

---
+++ CalendarioTareas/CalendarioTareas/cTarea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarioTareas
+{
+    class cTarea
+    {
+        public int iDia { get; set; }
+        public int iMes { get; set; }
+        public int iAnio { get; set; }
+        public int iMateria { get; set; }
+        public string sMateria { get; set; }
+        public string sTarea { get; set; }
+        public bool bCompletada { get; set; }
+    }
+}
