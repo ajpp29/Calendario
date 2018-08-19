20745f95a511e50110e34f782b0860bdcf8b2315
Angel Jimenez <angeljpp29@gmail.com>
2018-08-19 08:43:08 -0600

    Modificación de variables y creacio de constructores en la clase Tarea

--- CalendarioTareas/CalendarioTareas/cTarea.cs
+++ CalendarioTareas/CalendarioTareas/cTarea.cs
@@ -8,12 +8,72 @@ namespace CalendarioTareas
 {
     class cTarea
     {
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
+        public cTarea()
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
+        /// <param name="Dia">Dia de entrega</param>
+        /// <param name="Mes">Mes de entrega</param>
+        /// <param name="Anio">Año de entrega</param>
+        /// <param name="Materia">Materia de la tarea</param>
+        /// <param name="Tarea">Especificación de la tarea</param>
+        public cTarea(int Dia,int Mes, int Anio, int Materia, string Tarea)
+        {
+            iDia = Dia;
+            iMes = Mes;
+            iAnio = Anio;
+            sMateria = ObtenerMateria(Materia);
+            sTarea = Tarea;
+            bCompletada = false;
+        }
+
         public int iDia { get; set; }
         public int iMes { get; set; }
         public int iAnio { get; set; }
-        public int iMateria { get; set; }
         public string sMateria { get; set; }
         public string sTarea { get; set; }
         public bool bCompletada { get; set; }
+
+        /// <summary>
+        /// Obtiene el nombre de la materia de la tarea
+        /// </summary>
+        /// <param name="Materia">Clave de la materia</param>
+        /// <returns>Nombre de la materia</returns>
+        public string ObtenerMateria(int Materia)
+        {
+            switch (Materia)
+            {
+                case 0:
+                    return "Estadistica Inferencial";
+
+                case 1:
+                    return "Estructura de Datos II";
+
+                case 2:
+                    return "Fisica III";
+
+                case 3:
+                    return "Ingles";
+
+                case 4:
+                    return "Juventud y Vida Saludable";
+
+                case 5:
+                    return "Manejo e Implementacion de Archivos";
+
+                case 6:
+                    return "Organización Computacional";
+
+                default:
+                    return "No se especifico";
+            }
+        }
     }
 }
